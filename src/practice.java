8a62d715f8845ccd274be872516103f65033742c
Ashley Cove <ash.cove99@gmail.com>
2022-05-22 16:51:17 +0100

    Added String method examples

--- src/practice.java
+++ src/practice.java
@@ -17,5 +17,35 @@ public class practice {
         for (String item : arrList) {
             System.out.println(item);
         }
+
+        //String Methods
+    /*
+    We have learned some of the string methods that come with the String class:
+    */
+
+        // length()
+
+        System.out.println(arrList.get(0).length());
+        // concat()
+
+        System.out.println(arr[0].concat(arrList.get(1)));
+
+        // indexOf()
+        System.out.println("Hello".indexOf("e"));
+        // charAt()
+        System.out.println("Hello".charAt(1));
+        // equals() / equalsIgnoreCase()
+        System.out.println("Hello".equals("Hello"));
+        System.out.println(("Hello".equalsIgnoreCase("hello")));
+        // substring()
+        System.out.println("Hello Ashley".substring(6));
+        // toUpperCase() / toLowerCase()
+        System.out.println("hello".toUpperCase());
     }
+
 }
+
+
+
+
+
